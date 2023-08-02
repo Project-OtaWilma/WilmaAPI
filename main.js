09791844eab9d375b162d190eede35ec8682a8f8
TuuKeZu <moilanentuukka5@gmail.com>
2023-08-02 12:30:45 +0300

    add cache for google-calendar-API so they stop rate-limiting me

--- main.js
+++ main.js
@@ -15,6 +15,8 @@ const homework = require('./routers/homework');
 const rooms = require('./routers/rooms');
 const absences = require('./routers/absences');
 
+const { authorize } = require('./google/authorize');
+
 const limiter = require('./routers/rate-limit');
 
 
@@ -41,7 +43,15 @@ app.use('/api/', homework, limiter.standard);
 app.use('/api/', rooms, limiter.standard);
 app.use('/api/', absences, limiter.standard);
 
-app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}...`);
-});
+// initialize google calendar authorization
+authorize()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Listening on ${PORT}...`);
+    });
+})
+.catch(err => {
+    throw err;
+})
+
 
